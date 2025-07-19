55b2b29e9fa7c0799449f2bafcfa16c00651c38d
Ramazan <ramazandjaparov@gmail.com>
2025-07-19 21:59:53 +0600

    feat: finished header component

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { Navigation } from "../Navigation/Navigation";
 import styles from "./Header.module.css";
 
@@ -5,6 +6,27 @@ export const Header = () => {
   return (
     <div>
       <Navigation />
+      <header className={styles.header}>
+        <div className={styles.header__content}>
+          <span className={styles.header__logo}>Wishbone+Partners</span>
+          <div className={styles.header__title}>
+            <h1>The home of beautiful architecture.</h1>
+          </div>
+          <div>
+            <p className={styles.header__paragraph}>
+              We are an architecture firm with a focus on beautiful but
+              functional design. At its heart, we believe design is about
+              usability and accessibility — these are the guiding principles for
+              our work. Read more about our previous projects, our process and
+              our team below.
+            </p>
+          </div>
+          <Link href="/" className={styles.header__link}>
+            Read more
+          </Link>
+        </div>
+        <div className={styles.header__image}></div>
+      </header>
     </div>
   );
 };
